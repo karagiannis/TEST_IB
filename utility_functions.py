cb67496a8c4be2ed5dae716d8dddc045157e010f
karagiannis <lasse.l.karagiannis@gmail.com>
2023-08-28 21:20:06 +0200

    divide_time_range does not work

--- utility_functions.py
+++ utility_functions.py
@@ -10,7 +10,7 @@ import logging
 import time
 import re
 
-logging.basicConfig(filename='app.log', level=logging.ERROR)
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
 
 def listAllCsvfilesPaths(top_directory):
@@ -65,6 +65,86 @@ def maximum_duration_time_when_requesting_bars(bar_size: str):
         if range_list[0][0] <= bar_size_seconds:
             largest_duration = duration
     return largest_duration
+
+def minimum_duration_time_when_requesting_bars(bar_size: str):
+    if bar_size in allowable_bar_sizes:
+        bar_size_seconds = allowable_bar_sizes[bar_size]
+    min_duration = None
+    for duration, range_list in allowable_duration.items():
+        if range_list[0][1] >= bar_size_seconds:
+            min_duration = duration
+    return min_duration
+
+def get_duration_seconds_from_duration_str(duration_str):
+    if duration_str == "60 S":
+        return 60
+    elif duration_str == "120 S":
+        return 120
+    elif duration_str == "1800 S":
+        return 1800
+    elif duration_str == "3600 S":
+        return 3600
+    elif duration_str == "14400 S":
+        return 14400
+    elif duration_str == "28800 S":
+        return 28800  
+    elif duration_str == "1 D":
+        return 24 * 60 * 60
+    elif duration_str == "2 D":
+        return 2 * 24 * 60 * 60
+    elif duration_str == "1 W":
+        return 7 * 24 * 60 * 60
+    elif duration_str == "1 M":
+        return 30 * 24 * 60 * 60
+    elif duration_str == "1 Y":
+        return 365 * 24 * 60 * 60
+    
+def duration_to_duration_str(duration_seconds):
+    # Reverse parsing of the allowable_duration dictionary is 
+    # employed to handle cases where the provided duration in seconds
+    # does not align with a valid IB duration lookback. 
+    # This approach ensures accurate mapping of duration_str to duration_seconds, 
+    # preventing potential inconsistencies.
+    for duration_str, duration in reversed(allowable_duration.items()):
+        if duration[0][0] <= duration_seconds <= duration[0][1]:
+            return duration_str
+    raise ValueError("Invalid duration_seconds value inside duration_to_duration_str")
+
+
+    
+def get_finer_granularity_duration_in_seconds(duration_str):
+
+
+    # Step 1: Convert keys to a list
+    allowable_duration_keys = list(allowable_duration.keys())
+
+    # Step 2: Find index of max_duration key
+    max_duration_index = allowable_duration_keys.index(duration_str)
+
+    # Step 3: Get the key of the finer granularity duration
+    finer_granularity_key = allowable_duration_keys[max_duration_index - 1]
+
+    # Step 4: Retrieve finer granularity duration
+    finer_granularity_duration = allowable_duration[finer_granularity_key][0][1]
+
+    return finer_granularity_duration
+
+def get_finer_granularity_duration_as_string(duration_str):
+
+
+    # Step 1: Convert keys to a list
+    allowable_duration_keys = list(allowable_duration.keys())
+
+    # Step 2: Find index of max_duration key
+    max_duration_index = allowable_duration_keys.index(duration_str)
+
+    # Step 3: Get the key of the finer granularity duration
+    finer_granularity_key = allowable_duration_keys[max_duration_index - 1]
+
+
+    return finer_granularity_key
+
+    
 
 
 def last_OHLC_date_from_csv_file(file_path):
@@ -190,7 +270,7 @@ def datetime_six_months_ago_NewYork_time():
     while six_months_ago.weekday() != 4:
         six_months_ago = six_months_ago + timedelta(days=1)
         six_months_ago = six_months_ago.replace(hour=23, minute=59, second=59)  # replace works on the datetime object!
-    return six_months_ago  # Example "20230223 23:59:59 US/Eastern"
+    return six_months_ago  # 
 
 
 def get_last_friday_date():
@@ -206,7 +286,7 @@ def get_last_friday_date():
 
 
 # Here we assume that we have made a maximum duration request for a particular
-# bar_size and make take into account the max duration to calculate a
+# bar_size and we must take into account the max duration to calculate a
 # new end_date_time because the IBAPI does only allow you to specify a
 # an end_date_time and a duration which is backwards in time.
 # end_date_time = duration + start_time, where start_time is
@@ -215,7 +295,35 @@ def next_datetime_for_bar_request(end_datetime, bar_size):
     eastern_tz = pytz.timezone('US/Eastern')
     current_time = datetime.now(eastern_tz)
     duration = maximum_duration_time_when_requesting_bars(bar_size)
-    new_end_time = end_datetime + timedelta(seconds=duration)
+    if duration == "1 Y":
+        duration = timedelta(days=365)
+    elif duration == "1 M":
+        duration = timedelta(days=30)
+    elif duration == "1 W":
+        duration = timedelta(weeks=1)
+    elif duration == "2 D":
+        duration = timedelta(days=2)
+    elif duration == "1 D":
+        duration = timedelta(days=1)
+    elif duration == "28800 S":
+        duration = timedelta(seconds=28800)
+    elif duration == "14400 S":
+        duration = timedelta(seconds=14400)
+    elif duration == "3600 S":
+        duration = timedelta(seconds=3600)
+    elif duration ==  "1800 S":
+        duration = timedelta(seconds=1800)
+    elif duration == "120 S":
+        duration = timedelta(seconds=120)
+    elif duration ==  "60 S":
+        duration = timedelta(seconds=60)
+    else:
+        raise ValueError(f"Invalid duration: {duration}")
+
+    new_end_time = end_datetime + duration
+    # Localize new_end_time with the same timezone as current_time
+    #new_end_time = eastern_tz.localize(new_end_time)
+    
     if new_end_time > current_time:
         return current_time
     else:
@@ -223,55 +331,141 @@ def next_datetime_for_bar_request(end_datetime, bar_size):
 
 
 def generate_update_list(top_directory: str):
+    logging.debug("Starting generate_update_list function")
     collection_csv_file_paths = listAllCsvfilesPaths(top_directory)
+    logging.debug(f"Found {len(collection_csv_file_paths)} CSV files")
     collection_need_of_update = []
-    six_months_ago = datetime_six_moths_ago_NewYork_time()
+    six_months_ago = datetime_six_months_ago_NewYork_time()
     last_friday = get_last_friday_date()
     now = datetime_now_NewYork_time()
 
     for csv_file_path in collection_csv_file_paths:
+        logging.debug(f"Processing CSV file: {csv_file_path}")
         bar_size = bar_size_from_file_path(csv_file_path)
         allowable_duration_in_bar_request = maximum_duration_time_when_requesting_bars(bar_size)
         instrument = get_instrument_from_file_name(csv_file_path)
-        contract = get_contract_from_csv_file_name(csv_file_path)
+        contract = get_contract_from_csv_file_path(csv_file_path)
 
         if file_is_empty(csv_file_path):
             start_datetime = six_months_ago
         else:
-            last_saved_bar_datetime = last_OHLC_date_from_csv_file(csv_file_path)
-            start_datetime = next_datetime_for_bar_request(last_saved_bar_datetime, bar_size)
+            last_saved_bar_datetime_str = last_OHLC_date_from_csv_file(csv_file_path)
+            last_saved_bar_datetime_str=last_saved_bar_datetime_str.split('.')[0]
+
+            eastern_tz = pytz.timezone('US/Eastern')
+            last_saved_bar_datetime = eastern_tz.localize(datetime.strptime(last_saved_bar_datetime_str, "%Y-%m-%d %H:%M:%S"))
+            start_datetime = last_saved_bar_datetime
+            end_datetime = next_datetime_for_bar_request(last_saved_bar_datetime, bar_size)
 
         if todays_week_day() == 5 or todays_week_day() == 6:
             end_datetime = last_friday
         else:
             end_datetime = now
 
+        actual_duration = (end_datetime-start_datetime).total_seconds()
+        logging.debug(f"Calculated actual_duration: {actual_duration}")
         update_dict = {
             'start_datetime': start_datetime,
             'end_datetime': end_datetime,
+            'actual_duration':actual_duration,
+            "max_allowable_duration_look_back": get_duration_seconds_from_duration_str(\
+                maximum_duration_time_when_requesting_bars(bar_size)),
             'contract': contract,
             'instrument': instrument,
             'bar_size': bar_size,
             'max_duration': allowable_duration_in_bar_request,
             'file_path': csv_file_path
         }
+        logging.debug(f"Generated update_dict: {update_dict}")
 
         collection_need_of_update.append(update_dict)
-
+    logging.debug("Completed generate_update_list function")
     return collection_need_of_update
 
 
 
-def divide_time_range(start_datetime, end_datetime, bar_size):
-    total_duration = (end_datetime - start_datetime).total_seconds()
-    restricted_duration = maximum_duration_time_when_requesting_bars(bar_size)
-    num_chunks = int(total_duration / restricted_duration)
+def divide_time_range(start_datetime: datetime, end_datetime: datetime, bar_size: str):
+    logging.debug("Starting divide_time_range function")
 
+    # Calculate the total duration of the requested time range
+    total_duration = (end_datetime - start_datetime).total_seconds()
+
+    # Get the maximum allowed duration lookback from IB based on bar size
+    restricted_duration_str = maximum_duration_time_when_requesting_bars(bar_size)
+    logging.debug(f"Maximum allowed duration lookback acquired from Interactive Brokers: {restricted_duration_str}")
+
+    # Convert the maximum allowed duration to seconds
+    restricted_duration = get_duration_seconds_from_duration_str(restricted_duration_str)
+    logging.debug(f"Maximum allowed duration lookback in seconds: {restricted_duration}")
+
+    # Initialize the list to store duration lookback requests
+    duration_lookback_request_list = []
+
+    # Check if the total duration is greater than or equal to the maximum allowed duration
+    if total_duration >= restricted_duration:
+        logging.debug("Total duration is greater than or equal to the restricted duration")
+        duration_lookback_request_list.append(restricted_duration_str)
+        reminder_for_finer_duration = total_duration - restricted_duration
+        new_total_duration = total_duration
+    else:
+        # If total duration is less than restricted duration, set initial values for the loop
+        #We are priming reminder_for_finer_duration for the beginning of the while loop
+        reminder_for_finer_duration_str = duration_to_duration_str(total_duration)
+        reminder_for_finer_duration = get_duration_seconds_from_duration_str(reminder_for_finer_duration_str)
+
+        new_total_duration = total_duration
+
+    # Get the minimum duration for the specified bar size
+    min_duration_for_barsize = get_duration_seconds_from_duration_str(minimum_duration_time_when_requesting_bars(bar_size))
+
+    # Continue until the reminder becomes smaller than the minimum duration for the specified bar size
+    while not (reminder_for_finer_duration < min_duration_for_barsize):
+
+        finer_granularity_duration = reminder_for_finer_duration
+        finer_granularity_duration_str = duration_to_duration_str(finer_granularity_duration)
+        # Calculate the number of divisions of the current duration
+        num_divisions_of_current_duration = int(new_total_duration / finer_granularity_duration)
+        print("num_divisions_of_current_duration:",num_divisions_of_current_duration)
+
+        # Calculate the reminder for finer duration
+        reminder_for_finer_duration = new_total_duration % finer_granularity_duration
+
+        # Check if divisions are possible without a reminder
+        if reminder_for_finer_duration == 0:
+            for division_index in range(num_divisions_of_current_duration):
+                duration_lookback_request_list.append(finer_granularity_duration_str)
+            break
+
+        # Check if divisions are possible with a reminder
+        elif reminder_for_finer_duration != 0:
+            for division_index in range(num_divisions_of_current_duration):
+                duration_lookback_request_list.append(finer_granularity_duration_str)
+            # Get the next finer granularity duration and update values
+            new_total_duration = reminder_for_finer_duration
+            finer_granularity_duration_str = get_finer_granularity_duration_as_string(finer_granularity_duration_str)
+            logging.debug(f"Next allowed duration lookback is: {finer_granularity_duration_str}")
+            finer_granularity_duration = get_duration_seconds_from_duration_str(finer_granularity_duration_str)
+            logging.debug(f"Next allowed duration lookback is (in sec): {finer_granularity_duration}")
+            # If finer granularity becomes smaller than the minimum duration, add the minimum duration and exit
+            if finer_granularity_duration < min_duration_for_barsize:
+                duration_lookback_request_list.append(minimum_duration_time_when_requesting_bars(bar_size))
+                break
+
+    # Initialize the list to store chunks of time
     chunks = []
-    for i in range(num_chunks):
-        end_chunk_time = start_datetime + timedelta(seconds=restricted_duration)
-        chunks.append((start_datetime, end_chunk_time))
-        start_datetime = end_chunk_time
+    chunk_end = end_datetime
+    chunk ={}
+    # Construct chunks by going backwards in time using duration lookback requests
+    for duration_str in duration_lookback_request_list:
+        duration = get_duration_seconds_from_duration_str(duration_str)
+        chunk_start = chunk_end - timedelta(seconds=duration)
+        chunk['chunk_start']=chunk_start
+        chunk['chunk_end']=chunk_end
+        chunk['chunk_duration_str']=duration_str
+        chunk['chunk_duration']=duration
+        chunks.append(chunk)
+        chunk_end = chunk_start
+
     return chunks
 
 
